7132aa502cf4747d8299e77199a6d660f9889215
Velzkid <velzcielz@gmail.com>
2025-09-22 18:46:45 +0800

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,6 @@
+def greet_user(message: str) -> str:
+    """
+    Dummy response function for Velz Assistant.
+    Bisa diganti nanti dengan AI model / API call.
+    """
+    return f"I received your message -> {message}"
